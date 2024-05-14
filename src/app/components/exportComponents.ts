56073b00fcc429246792d6ecc2859aa9ccf32bed
Gillhad <crbaronv@gmail.com>
2024-05-14 13:05:02 +0200

    add prettier titles change food design

--- src/app/components/exportComponents.ts
+++ src/app/components/exportComponents.ts
@@ -1,5 +1,7 @@
 import { MainHeaderComponent } from './main-header/main-header/main-header.component';
 import { FooterComponentComponent } from './footer/footer-component/footer-component.component';
+import { TitleContaineredComponent } from './title-containered/title-containered.component';
 
+///Export common views for many pages
 
-export const commonComponents =  [FooterComponentComponent, MainHeaderComponent]
+export const commonComponents =  [FooterComponentComponent, MainHeaderComponent, TitleContaineredComponent]
--- src/app/food/food-view/food-view/food-view.component.ts
+++ src/app/food/food-view/food-view/food-view.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MainHeaderComponent } from '../../../components/main-header/main-header/main-header.component';
 import { FooterComponentComponent } from '../../../components/footer/footer-component/footer-component.component';
+import { commonComponents } from '../../../components/exportComponents';
 
 @Component({
   selector: 'food-view',
   standalone: true,
-  imports: [MainHeaderComponent, FooterComponentComponent],
+  imports: [commonComponents, FooterComponentComponent],
   templateUrl: './food-view.component.html',
   styleUrl: './food-view.component.scss'
 })
