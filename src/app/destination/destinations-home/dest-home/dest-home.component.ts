4e3aeb69b92003baeeea34af2adfbf36cff3941a
Gillhad <crbaronv@gmail.com>
2024-05-14 13:13:48 +0200

    fix proper hover on recent-post

--- src/app/destination/destinations-home/dest-home/dest-home.component.ts
+++ src/app/destination/destinations-home/dest-home/dest-home.component.ts
@@ -3,14 +3,25 @@ import { MainHeaderComponent } from '../../../components/main-header/main-header
 import { FooterComponentComponent } from '../../../components/footer/footer-component/footer-component.component';
 import { commonComponents } from '../../../components/exportComponents';
 import { RouterModule } from '@angular/router';
+import { MapComponentComponent } from '../../../components/map-component/map-component/map-component.component';
+import { CountriesHelperService } from '../../../../services/countries-helper.service';
+import { Countries } from '../../../../models/countries';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-dest-home',
   standalone: true,
-  imports: [MainHeaderComponent,commonComponents,RouterModule],
+  imports: [MainHeaderComponent,commonComponents,RouterModule, MapComponentComponent, NgFor],
   templateUrl: './dest-home.component.html',
   styleUrl: './dest-home.component.scss'
 })
 export class DestHomeComponent {
+
+
+  public mapCountries:Countries[] = [];
+
+constructor(private _countryService:CountriesHelperService){
+  this.mapCountries = _countryService.getCountries();
+}
 
 }
